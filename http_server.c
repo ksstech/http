9a05a56d037a6a11641b9a33eaf075c13775d7b2
André Maree <andrem@kss.co.za>
2019-09-19 08:50:19 +0200

    Improve debug tracking and reporting

--- http_server.c
+++ http_server.c
@@ -49,7 +49,7 @@
 
 // ############################### BUILD: debug configuration options ##############################
 
-#define	debugFLAG						0x0000
+#define	debugFLAG						0x2000
 
 #define	debugTRACK						(debugFLAG & 0x2000)
 #define	debugPARAM						(debugFLAG & 0x4000)
@@ -257,7 +257,7 @@ void	vHttpServerCloseClient(netx_t * psCtx) {
 	vRtosClearStatus(flagNET_HTTP_CLNT) ;
 	HttpState = stateHTTP_WAITING ;
 	xNetClose(psCtx) ;
-	IF_CPRINT(debugTRACK, "closing\n") ;
+	IF_CTRACK(debugTRACK, "closing\n") ;
 }
 
 /**
@@ -395,7 +395,7 @@ int32_t	xHttpServerResponseHandler(http_parser * psParser) {
  * 8. Respond to /restart (as emergency)
  */
 void	vTaskHttp(void * pvParameters) {
-	IF_SL_DBG(debugAPPL_THREADS, debugAPPL_MESS_UP) ;
+	IF_CTRACK(debugAPPL_THREADS, debugAPPL_MESS_UP) ;
 	sRR.sBuf.pBuf	= malloc(sRR.sBuf.Size = httpSERVER_BUFSIZE) ;
 	HttpState 		= stateHTTP_INIT ;
 	vRtosSetStateRUN(taskHTTP) ;
@@ -405,7 +405,7 @@ void	vTaskHttp(void * pvParameters) {
 		switch(HttpState) {
 		int32_t	iRetVal ;
 		case stateHTTP_DEINIT:
-			IF_CPRINT(debugTRACK, "de-init\n") ;
+			IF_CTRACK(debugTRACK, "de-init\n") ;
 			vRtosClearStatus(flagNET_HTTP_SERV | flagNET_HTTP_CLNT) ;
 			xNetClose(&sRR.sCtx) ;
 			xNetClose(&sServHttpCtx) ;
@@ -413,7 +413,7 @@ void	vTaskHttp(void * pvParameters) {
 			/* no break */
 
 		case stateHTTP_INIT:
-			IF_CPRINT(debugTRACK, "init\n") ;
+			IF_CTRACK(debugTRACK, "init\n") ;
 			memset(&sServHttpCtx, 0 , sizeof(netx_t)) ;
 			sServHttpCtx.sa_in.sin_family		= AF_INET ;
 			sServHttpCtx.type					= SOCK_STREAM ;
@@ -431,7 +431,7 @@ void	vTaskHttp(void * pvParameters) {
 			}
 			vRtosSetStatus(flagNET_HTTP_SERV) ;
 			HttpState = stateHTTP_WAITING ;
-			IF_CPRINT(debugTRACK, "waiting\n") ;
+			IF_CTRACK(debugTRACK, "waiting\n") ;
 			/* no break */
 
 		case stateHTTP_WAITING:
@@ -450,13 +450,13 @@ void	vTaskHttp(void * pvParameters) {
 			}
 			vRtosSetStatus(flagNET_HTTP_CLNT) ;			// mark as having a client connection
 			HttpState = stateHTTP_CONNECTED ;
-			IF_CPRINT(debugTRACK, "connected\n") ;
+			IF_CTRACK(debugTRACK, "connected\n") ;
 			/* no break */
 
 		case stateHTTP_CONNECTED:
 			iRetVal = xNetRead(&sRR.sCtx, sRR.sBuf.pBuf, sRR.sBuf.Size) ;
 			if (iRetVal > 0) {							// read something ?
-				IF_CPRINT(debugTRACK, "start parsing\n") ;
+				IF_CTRACK(debugTRACK, "start parsing\n") ;
 				if (sServHttpCtx.maxRx < iRetVal) {		// yes, update the Rx packet stats
 					sServHttpCtx.maxRx = iRetVal ;
 				}
@@ -481,26 +481,26 @@ void	vTaskHttp(void * pvParameters) {
 				sRR.sBuf.Used		= iRetVal ;
 				iRetVal = xHttpCommonDoParsing(&sParser) ;
 				if (iRetVal > 0) {						// build response if something was parsed....
-					IF_CPRINT(debugTRACK, "start response handler\n") ;
+					IF_CTRACK(debugTRACK, "start response handler\n") ;
 					xStdOutLock(portMAX_DELAY) ;
 					iRetVal = xHttpServerResponseHandler(&sParser) ;
 					xStdOutUnLock() ;
 				}
-				IF_CPRINT(debugTRACK, "Parsing done\n") ;
+				IF_CTRACK(debugTRACK, "Parsing done\n") ;
 				// socket closed or error occurred or coClose was set, close the connection
 				if (iRetVal == 0 || 					// nothing parsed or socket closed?
 					sRR.sCtx.error != 0 || 				// any error (even EAGAIN) on write ?
 					sRR.hvConnect == coClose) {			// or connection must be closed ?
 					vHttpServerCloseClient(&sRR.sCtx) ;	// then close the damn thing
-					IF_CPRINT(debugTRACK, "client closed\n") ;
+					IF_CTRACK(debugTRACK, "client closed\n") ;
 				} else {
 					// both parse & response handler & write was successful
 				}
 			} else if (sRR.sCtx.error != EAGAIN) {		// not EAGAIN/EWOULDBLOCK, socket closed OR real error
 				vHttpServerCloseClient(&sRR.sCtx) ;
-				IF_CPRINT(debugTRACK, "client closed\n") ;
+				IF_CTRACK(debugTRACK, "client closed\n") ;
 			}
-			IF_CPRINT(debugTRACK, "Tx done\n") ;
+			IF_CTRACK(debugTRACK, "Tx done\n") ;
 			break ;
 
 		default:	SL_ERR(debugAPPL_PLACE) ;
@@ -510,7 +510,7 @@ void	vTaskHttp(void * pvParameters) {
 	vPortFree(sRR.sBuf.pBuf) ;
 	xNetClose(&sServHttpCtx) ;
 	xNetClose(&sRR.sCtx) ;
-	IF_SL_DBG(debugAPPL_THREADS, debugAPPL_MESS_DN) ;
+	IF_CTRACK(debugAPPL_THREADS, debugAPPL_MESS_DN) ;
 	vTaskDelete(NULL) ;
 }
 
@@ -518,12 +518,12 @@ void	vTaskHttpInit(void) { xRtosTaskCreate(vTaskHttp, "HTTP", httpSTACK_SIZE, NU
 
 void	vHttpReport(void) {
 	if (xRtosCheckStatus(flagNET_HTTP_CLNT)) {
-		xNetReport(&sRR.sCtx, __FUNCTION__, 0, 0, 0) ;
+		xNetReport(&sRR.sCtx, "HTTPclt", 0, 0, 0) ;
 	}
 	if (xRtosCheckStatus(flagNET_HTTP_SERV)) {
-		xNetReport(&sServHttpCtx, __FUNCTION__, 0, 0, 0) ;
+		xNetReport(&sServHttpCtx, "HTTPsrv", 0, 0, 0) ;
 	} else {
-		PRINT("%C%-15s%C ", xpfSGR(attrRESET, colourFG_CYAN, 0, 0), __FUNCTION__, attrRESET) ;
+		PRINT("%CHTTP%C\t", xpfSGR(attrRESET, colourFG_CYAN, 0, 0), attrRESET) ;
 	}
-	PRINT("\t\tFSM=%d  maxTX=%u  maxRX=%u\n", HttpState, sServHttpCtx.maxTx, sServHttpCtx.maxRx) ;
+	PRINT("\tFSM=%d  maxTX=%u  maxRX=%u\n", HttpState, sServHttpCtx.maxTx, sServHttpCtx.maxRx) ;
 }
