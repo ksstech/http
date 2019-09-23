71f44be324d7b0ddc89e836abf109a90705e5d5a
André Maree <andrem@kss.co.za>
2019-09-23 17:52:50 +0200

    Changed to renamed flag names to clearly identify STA vs AP modes

--- http_server.c
+++ http_server.c
@@ -401,7 +401,7 @@ void	vTaskHttp(void * pvParameters) {
 	vRtosSetStateRUN(taskHTTP) ;
 
 	while (xRtosVerifyState(taskHTTP)) {
-		xRtosWaitStatus(flagNET_L1 | flagNET_L2 | flagNET_L3, portMAX_DELAY) ;	// ensure IP is up and running...
+		xRtosWaitStatus(flagL3_STA, portMAX_DELAY) ;	// ensure IP is up and running...
 		switch(HttpState) {
 		int32_t	iRetVal ;
 		case stateHTTP_DEINIT:
