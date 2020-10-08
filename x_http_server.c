314ccecf7a0b099213b603fad22c8c61855f2ecf
André Maree <ammaree@users.noreply.github.com>
2020-10-08 15:34:19 +0200

    Rename RTC based variables

--- x_http_server.c
+++ x_http_server.c
@@ -43,6 +43,7 @@
 #include	"hal_fota.h"
 #include	"hal_storage.h"
 #include	"hal_mcu.h"									// for halMCU_Restart()
+#include	"hal_nvs.h"
 
 #include	<stdlib.h>
 #include	<string.h>
@@ -234,15 +235,15 @@ int32_t	xHttpHandle_API(http_parser * psParser) {
 			vCommandInterpret((int) CHR_CR, false) ;
 		}
 	}
-	if (xUBufAvail(&sNVmem.sNVbuf) > 0) {
+	if (xUBufAvail(&sRTCvars.sRTCbuf) > 0) {
 		/* Definitive problem here if the volume of output from vCommandInterpret() exceed the
 		 * size of the buffer. In this case some content would have been overwritten and the
 		 * pointers would have wrapped and could point somewhere other than the start of the
 		 * buffer. In this case, the response as sent will be incomplete or invalid.
 		 * XXX change handling to accommodate sending 2 separate blocks
 		 */
-		iRV = xHttpSendResponse(psParser, format, xUBufAvail(&sNVmem.sNVbuf), pcUBufTellRead(&sNVmem.sNVbuf)) ;
-		vUBufReset(&sNVmem.sNVbuf) ;
+		iRV = xHttpSendResponse(psParser, format, xUBufAvail(&sRTCvars.sRTCbuf), pcUBufTellRead(&sRTCvars.sRTCbuf)) ;
+		vUBufReset(&sRTCvars.sRTCbuf) ;
 	} else {
 		iRV = xHttpSendResponse(psParser, "<html><body><h2>Command completed</h2></body></html>") ;
 	}
