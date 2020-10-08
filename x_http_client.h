d23f012dc84b9dda29479bcd35f4a500aa923854
André Maree <ammaree@users.noreply.github.com>
2020-10-08 15:36:32 +0200

    Cleanup #include files, move HTTP API URL's and keys to suitable locations

--- x_http_client.h
+++ x_http_client.h
@@ -4,85 +4,22 @@
 
 #pragma once
 
-#include	"x_config.h"								// required for API keys...
 #include 	"x_http_common.h"
-#include	"x_sockets.h"
-#include	"x_definitions.h"
-#include	"x_time.h"
 
-#include	"stdint.h"
-#include	"stdbool.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include	<stdint.h>
+#include	<stdbool.h>
 
 // ###################################### BUILD : CONFIG definitions ###############################
 
 
 // ########################################### macros #############################################
 
-/*
- * Open Weather Map service definitions
- */
-#ifndef		configHTTP_APIKEY_OPEN_WEATHERMAP
-	#error	" Open WeatherMap : API key undefined, normally in x_config.h in application directory"
-#endif
-
-#define	configHTTP_HOST_OPEN_WEATHERMAP			"api.openweathermap.org"
-#define	configHTTP_FCAST_OPEN_WEATHERMAP		"GET /data/2.5/forecast/?q=Johannesburg,ZA"
-#define	configHTTP_FCAST_JOHANNESBURG			configHTTP_FCAST_OPEN_WEATHERMAP"&APPID="configHTTP_APIKEY_OPEN_WEATHERMAP
-
-// https://www.iplocation.net/
-
-/*
- * EurekAPI.com (basic free 30day, 2017/06/06)
- */
-#ifndef		configHTTP_APIKEY_EUREKAPI
-	#error	" EurekAPI.com: API key undefined, normally in x_config.h in application directory"
-#endif
-
-#define	configHTTP_HOST_EUREKAPI				"api.eurekapi.com"
-#define	configHTTP_QUERY_EUREKAPI				"GET /iplocation/v1.8/locateip?key="configHTTP_APIKEY_EUREKAPI"&ip=196.201.119.114&format=JSON&compact=Y"
-
-/*
- * free geo-ip service definition
- */
-#define	configHTTP_HOST_FREE_GEO_IP				"freegeoip.net"
-#define	configHTTP_FORMAT_FREE_GEO_IP			"GET /json"
-
-/*
- * Google geolocation API
- *		https://developers.google.com/maps/documentation/geolocation/intro
- * Example:
- *		https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
- */
-#define	googleAPI_GEOLOC_HOST					"www.googleapis.com"
-#define	googleAPI_GEOLOC_QUERY					"POST /geolocation/v1/geolocate?key="configHTTP_APIKEY_GOOGLE_PAIDKEY
-#define	googleAPI_GEOLOC_BODY					"{ }\r\n"
-
-/*
- * Google elevation API
- *		https://developers.google.com/maps/documentation/elevation/start
- * Example:
- *		https://maps.googleapis.com/maps/api/elevation/json?locations=39.7391536,-104.9847034&key=YOUR_API_KEY
- */
-//#define	googleAPI_ELEVATION_HOST				"maps.googleapis.com"
-#define	googleAPI_ELEVATION_QUERY				"GET /maps/api/elevation/json?locations=%.7f,%.7f&key="configHTTP_APIKEY_GOOGLE_PAIDKEY
-
-/*
- * Google TimeZone API
- *		https://developers.google.com/maps/documentation/timezone/intro#Requests
- * Example:
- * 		https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=YOUR_API_KEY
- */
-//#define	googleAPI_TIMEZONE_HOST					"maps.googleapis.com"
-#define	googleAPI_TIMEZONE_QUERY				"GET /maps/api/timezone/json?location=%.7f,%.7f&timestamp=%d&key="configHTTP_APIKEY_GOOGLE_PAIDKEY
 
 // ######################################### enumerations ##########################################
 
 
 // ######################################### structures ############################################
+
 
 // ################################### Global variables ############################################
 
