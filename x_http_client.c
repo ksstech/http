99eec721f040ba9050ca02e389444202888788ce
André Maree <ammaree@users.noreply.github.com>
2022-07-09 14:07:46 +0200

    Improve FOTA flow checking

--- x_http_client.c
+++ x_http_client.c
@@ -445,9 +445,11 @@ int xHttpClientCheckUpgrades(bool bCheck) {
 	 * #2 hardware platform: "device-specification-token.bin"
 	 * #3 to be defined
 	 */
-	if (allSYSFLAGS(sfRESTART) == 0)
 	int iRV = xHttpClientFirmwareUpgrade((void *) idSTA, bCheck);
+	if (iRV > erFAILURE && allSYSFLAGS(sfRESTART) == 0)
 		iRV = xHttpClientFirmwareUpgrade((void *) halDEV_UUID, bCheck);
+	if (iRV <= erFAILURE)
+		setSYSFLAGS(sfFW_OK);
 	if (bCheck == PERFORM)
 		SL_LOG(iRV < erSUCCESS ? SL_SEV_ERROR : SL_SEV_INFO, "FWupg %s", iRV < erSUCCESS ? "FAIL" : "Done");
 	return iRV;
