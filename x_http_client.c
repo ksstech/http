8ad4da1ee66b621beb6941d6f1ca7baa766b98ca
André Maree <ammaree@users.noreply.github.com>
2024-07-08 14:35:38 +0200

    Update x_http_client.c

--- x_http_client.c
+++ x_http_client.c
@@ -21,6 +21,7 @@
 // ############################### BUILD: debug configuration options ##############################
 
 #define	debugFLAG					0xF000
+#define debugDETAIL					(debugFLAG & 0x0001)
 #define	debugTIMING					(debugFLAG_GLOBAL & debugFLAG & 0x1000)
 #define	debugTRACK					(debugFLAG_GLOBAL & debugFLAG & 0x2000)
 #define	debugPARAM					(debugFLAG_GLOBAL & debugFLAG & 0x4000)
