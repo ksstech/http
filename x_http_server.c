66370d253a2646931bdc977e40f612401d6e9aef
André Maree <ammaree@users.noreply.github.com>
2025-03-06 12:57:00 +0200

    Update copyright date and/or message

--- x_http_server.c
+++ x_http_server.c
@@ -1,7 +1,4 @@
-/*
- * x_http_server.c s
- * Copyright (c) 2014-22 Andre M. Maree / KSS Technologies (Pty) Ltd.
- */
+// http_server.c - Copyright (c) 2014-25 Andre M. Maree / KSS Technologies (Pty) Ltd.
 
 #include "hal_platform.h"
 
